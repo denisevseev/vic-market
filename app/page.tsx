0b6822cfb5e34d8dc631b4a9de17df2717b4d0cc
Ajna Cancar <ajna.cancar2019@size.ba>
2024-02-02 11:44:46 +0100

    Setup project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home page</h1>
+      <Link href="/privacy-policy">
+        <Button variant="contained">Privacy policy</Button>
+      </Link>
+    </main>
+  );
+}
---
+++ theme/ThemeRegistry.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeOptions, ThemeProvider } from "@mui/material/styles";
+import { Poppins } from "next/font/google";
+import { NextAppDirEmotionCacheProvider } from "./EmotionCache";
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
+
+const themeOptions: ThemeOptions = {
+  typography: {
+    fontSize: 12,
+    fontFamily: poppins.style.fontFamily,
+  },
+  palette: {
+    background: {
+      default: "#fff",
+    },
+  },
+};
+
+const theme = createTheme(themeOptions);
+
+export default function ThemeRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <NextAppDirEmotionCacheProvider options={{ key: "mui" }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </NextAppDirEmotionCacheProvider>
+  );
+}
