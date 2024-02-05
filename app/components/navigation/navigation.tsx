e9bf94863ed2d8d52d4f73576b1077c3c36dbacd
aneskdev <akaricdev@gmail.com>
2024-02-05 14:00:40 +0100

    add navigation

---
+++ app/components/navigation/navigation.tsx
@@ -0,0 +1,72 @@
+// components/Navigation.tsx
+import Link from "next/link";
+import "./navigation.scss";
+import Image from "next/image";
+import { Button, InputAdornment, TextField } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import SelectCity from "../shared/select-city/select-city";
+
+const Navigation = () => {
+  return (
+    <div id="wrapper">
+      <div className="block left">
+        <Image
+          src="/logo.png"
+          className="ml-left-logo"
+          alt="Logo"
+          width={181}
+          height={41}
+        />
+      </div>
+      <div className="block center">
+        {/* <div>All Cities</div> */}
+        <div className="select-city-container">
+        <SelectCity/>
+        </div>
+        <TextField
+          sx={{
+            width: { xs: "90%", md: "60%" },
+            mx: 2,
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "8px",
+              "& .MuiInputBase-input::placeholder": {
+                fontSize: { xs: "10px", sm: "12px" },
+                lineHeight: { xs: "1.25", sm: "1.5" },
+              },
+            },
+            // ... other styles
+          }}
+          variant="outlined"
+          placeholder="Search for a product, category or service"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end" sx={{ borderRadius: "8px" }}>
+                <div style={{ borderRadius: "50%" }}>
+                  <SearchIcon style={{ color: "black" }} />
+                </div>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <div className="registered-users-container">
+          <div className="reg-users-box">
+            <p className="registered-users-text black-text">Registered Users</p>
+          </div>
+          <div className="number-of-users-box">
+            <p className="registered-users-text blue-text">1,07,64,614</p>
+          </div>
+        </div>
+      </div>
+      <div className="block right">
+        <p className="login-text">Login</p>
+        <Button variant="contained" style={{ backgroundColor: "#2A5182" }}>
+          Sign Up
+        </Button>
+        <MenuIcon className="mr-right-icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      {/* <h1>Home page</h1>
       <Link href="/privacy-policy">
         <Button variant="contained">Privacy policy</Button>
-      </Link>
+      </Link> */}
     </main>
   );
 }
