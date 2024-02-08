9cb7b2acf8a3c02165297a9f7e4328a4f9ccf224
aneskdev <akaricdev@gmail.com>
2024-02-08 11:16:50 +0100

    trending and trsuted section

--- app/components/shared/scrollable-tabs/scrollable-tabs.tsx
+++ app/components/shared/scrollable-tabs/scrollable-tabs.tsx
@@ -5,7 +5,139 @@ import "./scrollable-tabs.scss";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 
-const Slide = ({ title, imgSrc }: any) => {
+// Slide component
+const SlideBigger = ({
+  title,
+  imgSrc,
+  price,
+  pieces,
+  category,
+  size,
+  units,
+  liters,
+  kilograms,
+  meter,
+  square,
+  isSmallCarousel,
+}: any) => {
+  return (
+    <div style={{ padding: "0 8px" }}>
+      <div
+        style={{
+          width: "100%",
+          height: isSmallCarousel ? "300px" : "200px",
+          backgroundColor: "rgba(109, 103, 89, 0.06)",
+          border: "1px solid rgb(225, 230, 239)",
+          borderRadius: "8px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "16px",
+        }}
+      >
+        {imgSrc && (
+          <div
+            style={{
+              marginBottom: "16px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100px",
+              height: "100px",
+            }}
+          >
+            <Image
+              src={imgSrc}
+              alt={title}
+              width={100}
+              height={80}
+              style={{
+                // marginBottom: "16px",
+                marginLeft: "auto",
+                marginRight: "auto",
+                // marginTop: "5px",
+              }}
+            />
+          </div>
+        )}
+        <hr
+          style={{
+            width: "100%",
+            borderWidth: "0.5px",
+            borderColor: "rgba(0, 0, 0, 0.1)",
+            margin: "10px 0",
+          }}
+        />
+
+        <div className="text-part">
+          <p
+            style={{
+              color: "rgb(45, 56, 64)",
+              fontWeight: "500",
+              zIndex: 2,
+              position: "relative",
+              fontSize: "14px",
+              marginBottom: "8px",
+            }}
+          >
+            {title}
+          </p>
+          {/* Horizontal line */}
+
+          {/* Optional information */}
+          {price && (
+            <p style={{ fontSize: "14px", color: "rgba(0, 0, 0, 0.5)" }}>
+              ${price.toFixed(2)}
+            </p>
+          )}
+          {pieces && (
+            <p style={{ fontSize: "14px", color: "rgba(0, 0, 0, 0.5)" }}>
+              {pieces} pcs
+            </p>
+          )}
+          {category && (
+            <p style={{ fontSize: "14px", color: "rgba(0, 0, 0, 0.5)" }}>
+              Category: {category}
+            </p>
+          )}
+          {size && (
+            <p style={{ fontSize: "14px", color: "rgba(0, 0, 0, 0.5)" }}>
+              Size: {size}
+            </p>
+          )}
+          {units && (
+            <p style={{ fontSize: "14px", color: "rgba(0, 0, 0, 0.5)" }}>
+              Units: {units}
+            </p>
+          )}
+          {liters && (
+            <p style={{ fontSize: "14px", color: "rgba(0, 0, 0, 0.5)" }}>
+              Liters: {liters}
+            </p>
+          )}
+          {kilograms && (
+            <p style={{ fontSize: "14px", color: "rgba(0, 0, 0, 0.5)" }}>
+              Kilograms: {kilograms}
+            </p>
+          )}
+          {meter && (
+            <p style={{ fontSize: "14px", color: "rgba(0, 0, 0, 0.5)" }}>
+              Meter: {meter}
+            </p>
+          )}
+          {square && (
+            <p style={{ fontSize: "14px", color: "rgba(0, 0, 0, 0.5)" }}>
+              Square: {square}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const SlideSmaller = ({ title, imgSrc }: any) => {
   return (
     <div style={{ padding: "0 8px" }}>
       <div
@@ -27,7 +159,7 @@ const Slide = ({ title, imgSrc }: any) => {
             alt={title}
             width={100}
             height={100}
-            style={{ marginBottom: "16px" }}
+            // style={{ marginBottom: "16px" }}
           />
         )}
         {/* Horizontal line */}
@@ -59,54 +191,54 @@ const Slider = dynamic(() => import("react-slick").then((m) => m.default), {
   ssr: false,
 });
 
-function VariableWidth({ data, title }: any) {
+function VariableWidth({ data, title, isSmallCarousel }: any) {
   const slickSettings = {
     dots: false,
     infinite: false,
     speed: 1000,
     autoplay: false,
-    slidesToShow: 9,
+    slidesToShow: isSmallCarousel === "true" ? 3 : 9,
     responsive: [
       {
         breakpoint: 1550,
         settings: {
-          slidesToShow: 8,
+          slidesToShow: isSmallCarousel ? 3 : 8,
         },
       },
       {
         breakpoint: 1450,
         settings: {
-          slidesToShow: 7,
+          slidesToShow: isSmallCarousel ? 2 : 7,
         },
       },
       {
         breakpoint: 1300,
         settings: {
-          slidesToShow: 6,
+          slidesToShow: isSmallCarousel ? 2 : 6,
         },
       },
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow: 5,
+          slidesToShow: isSmallCarousel ? 2 : 5,
         },
       },
       {
         breakpoint: 975,
         settings: {
-          slidesToShow: 4,
+          slidesToShow: isSmallCarousel ? 1 : 4,
         },
       },
       {
         breakpoint: 750,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: isSmallCarousel ? 1 : 3,
         },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: isSmallCarousel ? 1 : 2,
         },
       },
       {
@@ -120,12 +252,44 @@ function VariableWidth({ data, title }: any) {
 
   return (
     <div className="slider-container">
-      <p style={{ textAlign: "start", marginLeft: "2rem", fontSize: '18px', color: '#082F4A', fontStyle: 'Helvetica' }}>{title}</p>
+      <p
+        style={{
+          textAlign: "start",
+          marginLeft: "2rem",
+          fontSize: "18px",
+          color: "#082F4A",
+          fontStyle: "Helvetica",
+        }}
+      >
+        {title}
+      </p>
       <div style={{ margin: "20px" }}>
         <Slider {...slickSettings}>
-          {data?.map((slide: any, index: any) => (
-            <Slide key={index} title={slide.title} imgSrc={slide.imgSrc} />
-          ))}
+          {data?.map((slide: any, index: any) =>
+            isSmallCarousel ? (
+              <SlideBigger
+                key={index}
+                title={slide.title}
+                imgSrc={slide.imgSrc}
+                price={slide.price}
+                pieces={slide.pieces}
+                category={slide.category}
+                size={slide.size}
+                units={slide.units}
+                liters={slide.liters}
+                kilograms={slide.kilograms}
+                meter={slide.meter}
+                square={slide.square}
+                isSmallCarousel={isSmallCarousel}
+              />
+            ) : (
+              <SlideSmaller
+                key={index}
+                title={slide.title}
+                imgSrc={slide.imgSrc}
+              />
+            )
+          )}
         </Slider>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,76 @@ import FeaturedProducts from "./components/features-products/featured-products";
 
 export default function Home() {
   const slidesData = [
-    { title: "Common", imgSrc: "/get-distributers.svg" },
-    { title: "Medical", imgSrc: "/agriculture.svg" },
-    { title: "Personal", imgSrc: "/chemicals.svg" },
-    { title: "Solar Panels", imgSrc: "/get-distributers.svg" },
-    { title: "Common", imgSrc: "/get-distributers.svg" },
-    { title: "Medical", imgSrc: "/agriculture.svg" },
-    { title: "Personal", imgSrc: "/chemicals.svg" },
-    { title: "Solar Panels", imgSrc: "/get-distributers.svg" },
-    { title: "Common", imgSrc: "/get-distributers.svg" },
+    {
+      title: "Common",
+      imgSrc: "/get-distributers.svg",
+      // price: 79.99,
+      // pieces: 50,
+      // category: "Electronics",
+      // size: "Medium",
+      // units: "pcs",
+      liters: "Liters",
+      kilograms: "Kilograms",
+      meter: "Meter",
+      // square: "Foot/Square Foots",
+    },
+    {
+      title: "Medical",
+      imgSrc: "/agriculture.svg",
+      liters: "Liters",
+      kilograms: "Kilograms",
+      meter: "Meter",
+      // square: "Foot/Square Foots",
+    },
+    {
+      title: "Personal",
+      imgSrc: "/chemicals.svg",
+      liters: "Liters",
+      kilograms: "Kilograms",
+      meter: "Meter",
+    },
+    {
+      title: "Solar Panels",
+      imgSrc: "/get-distributers.svg",
+      liters: "Liters",
+      kilograms: "Kilograms",
+      meter: "Meter",
+    },
+    {
+      title: "Common",
+      imgSrc: "/get-distributers.svg",
+      liters: "Liters",
+      kilograms: "Kilograms",
+      meter: "Meter",
+    },
+    {
+      title: "Medical",
+      imgSrc: "/agriculture.svg",
+      liters: "Liters",
+      kilograms: "Kilograms",
+      meter: "Meter",
+    },
+    {
+      title: "Personal",
+      imgSrc: "/chemicals.svg",
+      liters: "Liters",
+      kilograms: "Kilograms",
+      meter: "Meter",
+    },
+    {
+      title: "Solar Panels",
+      imgSrc: "/get-distributers.svg",
+      liters: "Liters",
+      kilograms: "Kilograms",
+      meter: "Meter",
+    },
+    {
+      title: "Common",
+      imgSrc: "/get-distributers.svg",
+      liters: "Liters",
+      kilograms: "Kilograms",
+      meter: "Meter",
+    },
     { title: "Medical", imgSrc: "/agriculture.svg" },
     { title: "Personal", imgSrc: "/chemicals.svg" },
     { title: "Solar Panels", imgSrc: "/get-distributers.svg" },
@@ -74,6 +135,23 @@ export default function Home() {
 
           <div style={{ marginBottom: "2rem" }}>
             <FeaturedProducts />
+          </div>
+
+          <div className="arriwals-trusted">
+            <div>
+              <VariableWidth
+                data={slidesData}
+                title="Trending Categories"
+                isSmallCarousel="true"
+              />
+            </div>
+            <div>
+              <VariableWidth
+                data={slidesData}
+                title="Victorum Capital Trusted"
+                isSmallCarousel="true"
+              />
+            </div>
           </div>
         </div>
       </Box>
