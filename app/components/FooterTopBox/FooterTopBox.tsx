da79a336b2672a8c54897013ef574dae87d2cad1
aneskdev <akaricdev@gmail.com>
2024-02-12 10:11:07 +0100

    clickable icons

--- app/components/FooterTopBox/FooterTopBox.tsx
+++ app/components/FooterTopBox/FooterTopBox.tsx
@@ -17,56 +17,82 @@ export default function FooterTopBox() {
   const gPartnerHeight = isMobile ? 66 : 100;
   return (
     <div className="main-container-top">
+      <div className="top-box-container">
+        <div className="two-images">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={logoWidth}
+            height={logoHeight}
+            priority
+          />
+          <Image
+            src="/g-partner.svg"
+            className="ml-left-logo"
+            alt="Logo"
+            width={logoWidth}
+            height={gPartnerHeight}
+            priority
+          />
+        </div>
 
-   
-    <div className="top-box-container">
-      <div className="two-images">
-        <Image
-          src="/logo.png"
-          alt="Logo"
-          width={logoWidth}
-          height={logoHeight}
-          priority
-        />
-        <Image
-          src="/g-partner.svg"
-          className="ml-left-logo"
-          alt="Logo"
-          width={logoWidth}
-          height={gPartnerHeight}
-          priority
-        />
+        <div className="follow-us">
+          <h3>Follow us</h3>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-icon"
+          >
+            <FacebookIcon />
+          </a>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-icon"
+          >
+            <InstagramIcon />
+          </a>
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-icon"
+          >
+            <TwitterIcon />
+          </a>
+          <a
+            href="https://www.youtube.com/c"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-icon"
+          >
+            <YouTubeIcon />
+          </a>
+        </div>
+        <div className="btn-container">
+          <Button
+            variant="contained"
+            sx={{
+              height: "48px",
+              borderRadius: "8px",
+              backgroundColor: "#2A5182",
+              width: "185px",
+              ".button-text p": {
+                textTransform: "none",
+              },
+            }}
+          >
+            <div className="button-text">
+              <p className="font-size-16">Contact us</p>
+              <ArrowForwardIcon />
+            </div>
+          </Button>
+        </div>
       </div>
 
-      <div className="follow-us">
-        <h3>Follow us</h3>
-        <FacebookIcon />
-        <InstagramIcon />
-        <TwitterIcon />
-        <YouTubeIcon />
-      </div>
-      <div className="btn-container">
-        <Button
-          variant="contained"
-          sx={{
-            height: "48px",
-            borderRadius: "8px",
-            backgroundColor: "#2A5182",
-            width: "185px",
-            ".button-text p": {
-              textTransform: "none",
-            },
-          }}
-        >
-          <div className="button-text">
-            <p className="font-size-16">Contact us</p>
-            <ArrowForwardIcon />
-          </div>
-        </Button>
-      </div>
-    </div>
-
-    <div className="follow-us-down">
+      <div className="follow-us-down">
         <FacebookIcon />
         <InstagramIcon />
         <TwitterIcon />
@@ -74,21 +100,21 @@ export default function FooterTopBox() {
       </div>
 
       <div className="btn-container-big">
-      <Button
-              variant="contained"
-              //   onClick={handleNextStep}
-              sx={{
-                ".button-text p": {
-                  textTransform: "none",
-                },
-              }}
-              className="continueFormButton"
-            >
-              <div className="button-text">
-                <p className="font-size-16">Contact us</p>
-                <ArrowForwardIcon />
-              </div>
-            </Button>
+        <Button
+          variant="contained"
+          //   onClick={handleNextStep}
+          sx={{
+            ".button-text p": {
+              textTransform: "none",
+            },
+          }}
+          className="continueFormButton"
+        >
+          <div className="button-text">
+            <p className="font-size-16">Contact us</p>
+            <ArrowForwardIcon />
+          </div>
+        </Button>
       </div>
     </div>
   );
