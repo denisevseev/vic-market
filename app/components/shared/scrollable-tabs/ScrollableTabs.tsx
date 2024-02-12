c0f535c87fefe9c8802fa871f8b752e0db1dde95
aneskdev <akaricdev@gmail.com>
2024-02-12 11:59:53 +0100

    fix new arrivals and trusted

--- app/components/shared/scrollable-tabs/ScrollableTabs.tsx
+++ app/components/shared/scrollable-tabs/ScrollableTabs.tsx
@@ -27,7 +27,7 @@ const SlideBigger = ({
         style={{
           width: "100%",
           height: isSmallCarousel ? "300px" : "200px",
-          backgroundColor: "rgba(109, 103, 89, 0.06)",
+          backgroundColor: "rgb(255, 255, 255)",
           border: "1px solid rgb(225, 230, 239)",
           borderRadius: "8px",
           display: "flex",
@@ -81,6 +81,14 @@ const SlideBigger = ({
               fontSize: "14px",
               marginBottom: "8px",
               textAlign: "center",
+              // Additional styles for fixed height and ellipsis
+              height: "20px", // Set a fixed height based on your design needs
+              overflow: "hidden", // Hide overflow
+              textOverflow: "ellipsis", // Show an ellipsis for overflowed text
+              display: "-webkit-box", // Necessary for the line clamp to work
+              WebkitLineClamp: 1, // Limit the text to 2 lines
+              WebkitBoxOrient: "vertical", // Display content vertically for the clamp to work
+              lineHeight: "20px", // Adjust based on your font size for proper line spacing
             }}
           >
             {title}
@@ -230,27 +238,26 @@ const SlideSmaller = ({ title, imgSrc }: any) => {
           }}
         />
         <p
-  style={{
-    color: "rgb(45, 56, 64)",
-    fontWeight: "500",
-    zIndex: 2,
-    position: "relative",
-    fontSize: "14px",
-    textAlign: "center",
-    paddingLeft: "10px",
-    paddingRight: "10px",
-    height: "40px", // Set a fixed height
-    overflow: "hidden", // Hide overflow
-    textOverflow: "ellipsis", // Show an ellipsis for overflowed text
-    display: "-webkit-box", // Use webkit-box to enable line clamping
-    WebkitLineClamp: 2, // Number of lines to show
-    WebkitBoxOrient: "vertical", // Orient the text vertically
-    lineHeight: "20px", // Set line height to manage spacing between lines
-  }}
->
-  {title}
-</p>
-
+          style={{
+            color: "rgb(45, 56, 64)",
+            fontWeight: "500",
+            zIndex: 2,
+            position: "relative",
+            fontSize: "14px",
+            textAlign: "center",
+            paddingLeft: "10px",
+            paddingRight: "10px",
+            height: "40px", // Set a fixed height
+            overflow: "hidden", // Hide overflow
+            textOverflow: "ellipsis", // Show an ellipsis for overflowed text
+            display: "-webkit-box", // Use webkit-box to enable line clamping
+            WebkitLineClamp: 2, // Number of lines to show
+            WebkitBoxOrient: "vertical", // Orient the text vertically
+            lineHeight: "20px", // Set line height to manage spacing between lines
+          }}
+        >
+          {title}
+        </p>
       </div>
     </div>
   );
