3be152752c9dd0753771043a04ee6bbac6121ab9
AboudSM <aboudmashlah@gmail.com>
2024-02-15 16:47:04 +0100

    New arrivals, trading trusted fixes and send inquiries modal.

--- app/components/shared/ScrollableTabs/ScrollableTabs.tsx
+++ app/components/shared/ScrollableTabs/ScrollableTabs.tsx
@@ -4,9 +4,12 @@ import "slick-carousel/slick/slick-theme.css";
 import "./ScrollableTabs.scss";
 import dynamic from "next/dynamic";
 import Image from "next/image";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import DateRangeIcon from "@mui/icons-material/DateRange";
 import EditLocationIcon from "@mui/icons-material/EditLocation";
+import SendIcon from "@mui/icons-material/Send";
+import { useState } from "react";
+import InquiryModal from "../../FeaturedProducts/InquiryModal/InquiryModal";
 
 const SlideUpComing = ({ title, imgSrc, date, location }: any) => {
   return (
@@ -50,6 +53,9 @@ const SlideUpComing = ({ title, imgSrc, date, location }: any) => {
 
 // Slide component
 const SlideBigger = (item: any) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const handleOpenModal = () => setModalOpen(true);
+  const handleCloseModal = () => setModalOpen(false);
   const data = item.data ?? null;
   console.log("slider bigger", item);
   return (
@@ -102,133 +108,45 @@ const SlideBigger = (item: any) => {
           }}
         />
 
+        <p className="productNameText">{data.productName} naziv</p>
+        {/* Horizontal line */}
         <div className="text-part">
-          <p
-            style={{
-              color: "rgb(45, 56, 64)",
-              fontWeight: "500",
-              zIndex: 2,
-              position: "relative",
-              fontSize: "14px",
-              marginBottom: "8px",
-              textAlign: "center",
-              // Additional styles for fixed height and ellipsis
-              height: "20px", // Set a fixed height based on your design needs
-              overflow: "hidden", // Hide overflow
-              textOverflow: "ellipsis", // Show an ellipsis for overflowed text
-              display: "-webkit-box", // Necessary for the line clamp to work
-              WebkitLineClamp: 1, // Limit the text to 2 lines
-              WebkitBoxOrient: "vertical", // Display content vertically for the clamp to work
-              lineHeight: "20px", // Adjust based on your font size for proper line spacing
-            }}
-          >
-            {data.productName} naziv
-          </p>
-          {/* Horizontal line */}
-
           {/* Optional information */}
           {data.productPrice && (
-            <p
-              style={{
-                fontSize: "14px",
-                color: "rgba(0, 0, 0, 0.5)",
-                textAlign: "center",
-              }}
-            >
-              ${data.productPrice.toFixed(2)}
-            </p>
+            <p className="productInfoText">${data.productPrice.toFixed(2)}</p>
           )}
           {data.quantity && (
-            <p
-              style={{
-                fontSize: "14px",
-                color: "rgba(0, 0, 0, 0.5)",
-                textAlign: "center",
-              }}
-            >
-              {data.quantity} QTY
-            </p>
+            <p className="productInfoText">{data.quantity} QTY</p>
           )}
-          {/*data.categoryName && (
-            <p
-              style={{
-                fontSize: "14px",
-                color: "rgba(0, 0, 0, 0.5)",
-                textAlign: "center",
-              }}
-            >
-              Category: {data.categoryName}
-            </p>
-          )*/}
+          {/* {data.categoryName && (
+            <p className="productInfoText">Category: {data.categoryName}</p>
+          )} */}
 
-          {data.size && (
-            <p
-              style={{
-                fontSize: "14px",
-                color: "rgba(0, 0, 0, 0.5)",
-                textAlign: "center",
-              }}
-            >
-              Size: {data.size}
-            </p>
-          )}
-          {data.units && (
-            <p
-              style={{
-                fontSize: "14px",
-                color: "rgba(0, 0, 0, 0.5)",
-                textAlign: "center",
-              }}
-            >
-              Units: {data.units}
-            </p>
-          )}
+          {data.size && <p className="productInfoText">Size: {data.size}</p>}
+          {data.units && <p className="productInfoText">Units: {data.units}</p>}
           {data.liters && (
-            <p
-              style={{
-                fontSize: "14px",
-                color: "rgba(0, 0, 0, 0.5)",
-                textAlign: "center",
-              }}
-            >
-              Liters: {data.liters}
-            </p>
+            <p className="productInfoText">Liters: {data.liters}</p>
           )}
           {data.kilograms && (
-            <p
-              style={{
-                fontSize: "14px",
-                color: "rgba(0, 0, 0, 0.5)",
-                textAlign: "center",
-              }}
-            >
-              Kilograms: {data.kilograms}
-            </p>
+            <p className="productInfoText">Kilograms: {data.kilograms}</p>
           )}
-          {data.meter && (
-            <p
-              style={{
-                fontSize: "14px",
-                color: "rgba(0, 0, 0, 0.5)",
-                textAlign: "center",
-              }}
-            >
-              Meter: {data.meter}
-            </p>
-          )}
+          {data.meter && <p className="productInfoText">Meter: {data.meter}</p>}
           {data.square && (
-            <p
-              style={{
-                fontSize: "14px",
-                color: "rgba(0, 0, 0, 0.5)",
-                textAlign: "center",
-              }}
-            >
-              Square: {data.square}
-            </p>
+            <p className="productInfoText">Square: {data.square}</p>
           )}
         </div>
       </div>
+      <button className="inquiryButtonProductSlider" onClick={handleOpenModal}>
+        <Typography>Send Inquiry</Typography>
+        <SendIcon />
+      </button>
+      <InquiryModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        productName={data.productName}
+        company={data.categoryName}
+        imgSrc={data.productImage ?? "/get-distributers.svg"}
+      />
     </div>
   );
 };
@@ -269,27 +187,7 @@ const SlideSmaller = ({ title, imgSrc }: any) => {
             margin: "10px 0",
           }}
         />
-        <p
-          style={{
-            color: "rgb(45, 56, 64)",
-            fontWeight: "500",
-            zIndex: 2,
-            position: "relative",
-            fontSize: "14px",
-            textAlign: "center",
-            paddingLeft: "10px",
-            paddingRight: "10px",
-            height: "40px", // Set a fixed height
-            overflow: "hidden", // Hide overflow
-            textOverflow: "ellipsis", // Show an ellipsis for overflowed text
-            display: "-webkit-box", // Use webkit-box to enable line clamping
-            WebkitLineClamp: 2, // Number of lines to show
-            WebkitBoxOrient: "vertical", // Orient the text vertically
-            lineHeight: "20px", // Set line height to manage spacing between lines
-          }}
-        >
-          {title}
-        </p>
+        <p className="productNameText">{title}</p>
       </div>
     </div>
   );
