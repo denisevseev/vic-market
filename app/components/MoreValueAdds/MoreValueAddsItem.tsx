8ea9ef435ebb4e211e3df372e0417a4bdd79c0f7
Inas Suljic <inas.suljic@gmail.com>
2024-04-16 17:14:04 +0200

    Update

--- app/components/MoreValueAdds/MoreValueAddsItem.tsx
+++ app/components/MoreValueAdds/MoreValueAddsItem.tsx
@@ -3,19 +3,31 @@ import "./MoreValueAdds.scss";
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { useRouter } from "next/navigation";
 
 export default function MoreValueAddsItem() {
+  const router = useRouter();
+  const handleNavigate = (path: string) => {
+    router.push(path);
+  };
+  const handleUrl = (url: any) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <Box className="addsItemMain">
       <Box className="addItemSingelMain">
         <Box className="victorumCapitalAndLearnMore">
           <Box sx={{ zIndex: 9 }}>
             <Typography className="victorumCapitalText">
-            Get the latest
+              Get the latest
             </Typography>
             <Typography className="victorumCapitalText">news!</Typography>
           </Box>
-          <Box className="learnMoreAndIcon">
+          <Box
+            className="learnMoreAndIcon"
+            onClick={() => handleNavigate("/newsletter")}
+          >
             <Typography className="learnMoreLink">Subscribe!</Typography>
             <Box className="arrowIconBox">
               <ArrowForwardIcon className="arrowRightIcon" />
@@ -28,11 +40,11 @@ export default function MoreValueAddsItem() {
         <Box className="victorumCapitalAndLearnMore">
           <Box sx={{ zIndex: 9 }}>
             <Typography className="victorumCapitalText">
-            Discover your
+              Discover your
             </Typography>
             <Typography className="victorumCapitalText">potential!</Typography>
           </Box>
-          <Box className="learnMoreAndIcon">
+          <Box className="learnMoreAndIcon" onClick={() => handleNavigate("/categories")}>
             <Typography className="learnMoreLink">Learn more </Typography>
             <Box className="arrowIconBox">
               <ArrowForwardIcon className="arrowRightIcon" />
@@ -44,14 +56,13 @@ export default function MoreValueAddsItem() {
       <Box className="addItemSingelMain">
         <Box className="victorumCapitalAndLearnMore">
           <Box sx={{ zIndex: 9 }}>
-            <Typography className="victorumCapitalText">
-            You got
-            </Typography>
-            <Typography className="victorumCapitalText">
-            questions?
-            </Typography>
+            <Typography className="victorumCapitalText">You got</Typography>
+            <Typography className="victorumCapitalText">questions?</Typography>
           </Box>
-          <Box className="learnMoreAndIcon">
+          <Box
+            className="learnMoreAndIcon"
+            onClick={() => handleNavigate("/contact-us?from=legal")}
+          >
             <Typography className="learnMoreLink">Get in touch! </Typography>
             <Box className="arrowIconBox">
               <ArrowForwardIcon className="arrowRightIcon" />
@@ -64,11 +75,16 @@ export default function MoreValueAddsItem() {
         <Box className="victorumCapitalAndLearnMore">
           <Box sx={{ zIndex: 9 }}>
             <Typography className="victorumCapitalText">
-            Cheap online
+              Cheap online
             </Typography>
-            <Typography className="victorumCapitalText">transactions!</Typography>
+            <Typography className="victorumCapitalText">
+              transactions!
+            </Typography>
           </Box>
-          <Box className="learnMoreAndIcon">
+          <Box
+            className="learnMoreAndIcon"
+            onClick={() => handleUrl("https://www.vicpayments.com")}
+          >
             <Typography className="learnMoreLink">Learn more </Typography>
             <Box className="arrowIconBox">
               <ArrowForwardIcon className="arrowRightIcon" />
